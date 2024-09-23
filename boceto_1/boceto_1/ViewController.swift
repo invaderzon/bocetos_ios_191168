60296f3a9efddd1c093008239abef9d73484be2f
alumno <alumno@PC-V202-07.uacj.mx>
2024-09-23 10:44:17 -0600

    Pantallas conectadas

--- boceto_1/boceto_1/ViewController.swift
+++ boceto_1/boceto_1/ViewController.swift
@@ -7,13 +7,24 @@
 
 import UIKit
 
+struct elemento: Codable{
+    let userId: Int
+    let id: Int
+    let title: String
+    let completed: Bool
+    
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view
     }
 
 
+    
+    @IBSegueAction func al_abrir_pantalla_citas(_ coder: NSCoder) -> ControladorPantallaCitas? {
+        return ControladorPantallaCitas(muro_texto: "Miaaaaaaauuuu", de_quien:"MamaCat",coder: coder)
+    }
 }
-
