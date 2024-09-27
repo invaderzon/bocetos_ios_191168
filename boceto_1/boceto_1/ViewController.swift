d7b6e135eb37672fa478e85fcfc6d533e8f5376c
alumno <alumno@PC-V202-07.uacj.mx>
2024-09-27 10:49:24 -0600

    Conectando pantallas y textos

--- boceto_1/boceto_1/ViewController.swift
+++ boceto_1/boceto_1/ViewController.swift
@@ -7,30 +7,43 @@
 
 import UIKit
 
-struct elemento: Codable{
-    let userId: Int
-    let id: Int
-    let title: String
-    let completed: Bool
-    
-}
-
 class ViewController: UIViewController {
     var cita_para_enviar: Cita = Cita(quien_lo_dijo: "Gato", que_dijo: "MIAAAAAAAUUUUU")
-    
     var citas_disponibles: GeneradorDeCitas = GeneradorDeCitas()
-
+    var numero_aleatorio: Int = Int.random(in: 0...25)
+    
     override func viewDidLoad() {
         citas_disponibles.generar_citas_falsas()
         super.viewDidLoad()
         // Do any additional setup after loading the view
-        
-        let valor = Int.random(in: 0...100)
+        actualizar_cantidad()
     }
-
+    
+    
+    func actualizar_cantidad(){
+            labelcito.text = String(citas_disponibles.citas_creadas.count)
+        }
 
     
     @IBSegueAction func al_abrir_pantalla_citas(_ coder: NSCoder) -> ControladorPantallaCitas? {
         return ControladorPantallaCitas(cita_para_citar: citas_disponibles.obtener_cita_aleatoria(), coder: coder)
     }
+    
+    @IBAction func volver_a_pantalla_inicio(segue: UIStoryboardSegue){
+        if let pantalla_agregar_citas = segue.source as? ControladorPantallaAgregarCita{ citas_disponibles.agregar_cita(pantalla_agregar_citas.cita_creada!)
+        }
+        
+        
+        //print(pantalla_citass?.cita_actual.texto)
+        
+        /*if let pantalla_citass = segue.source as? ControladorPantallaCitas { citas_disponibles.agregar_cita(pantalla_citass.cita_actual.texto, quien_lo_dijo: pantalla_citass.cita_actual.cita_actual.nombre)}
+         }
+         else{
+         print("Eso no era un objeto de tipo <Controllador ")
+         }*/
+        
+        actualizar_cantidad()
+    }
+    
 }
+
---
+++ boceto_1/boceto_1/controladores_de_vistas/ControladorPantallaAgregarCita.swift
@@ -0,0 +1,30 @@
+//
+//  ControladorPantallaAgregarCita.swift
+//  boceto_1
+//
+//  Created by alumno on 9/27/24.
+//
+
+import UIKit
+class ControladorPantallaAgregarCita: UIViewController{
+    var quien_lo_dice: String = ""
+    var que_dice: String = ""
+    
+    var cita_creada: Cita? = nil
+    
+    @IBOutlet weak var quien_lo_dijo_view: UITextField!
+    
+    @IBOutlet weak var que_es_lo_que_dijo: UITextField!
+    
+    @IBAction func agregar_cita_nueva(_ sender:UIButton){
+        cita_creada = Cita(quien_lo_dijo: quien_lo_dijo_view.text!, que_dijo: que_es_lo_que_dijo.text!)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+    
+}
