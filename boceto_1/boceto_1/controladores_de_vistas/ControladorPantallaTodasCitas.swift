73092ff97b35476494241874cd43248b47c396d7
alumno <alumno@lab_quimica.uacj.mx>
2024-10-04 10:15:02 -0600

    Avance para calificar

---
+++ boceto_1/boceto_1/controladores_de_vistas/ControladorPantallaTodasCitas.swift
@@ -0,0 +1,24 @@
+//
+//  ControladorPantallaTodasCitas.swift
+//  boceto_1
+//
+//  Created by alumno on 10/4/24.
+//
+
+import UIKit
+
+
+class ControladorPantallaTodasCitas: UIViewController{
+    
+    @IBOutlet weak var labelzote: UILabel!
+    
+    var citas_todas: Cita? = nil
+    
+    func mostrar_citas(){
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
