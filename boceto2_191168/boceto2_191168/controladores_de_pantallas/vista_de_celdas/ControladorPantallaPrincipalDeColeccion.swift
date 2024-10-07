76b893efadef5ebf577f79ad0ee2b64f34e4b878
alumno <alumno@lab_quimica.uacj.mx>
2024-10-07 10:25:55 -0600

    Bocetos 2

---
+++ boceto2_191168/boceto2_191168/controladores_de_pantallas/vista_de_celdas/ControladorPantallaPrincipalDeColeccion.swift
@@ -0,0 +1,96 @@
+//
+//  ControladorPantallaPrincipalDeColeccion.swift
+//  boceto2_191168
+//
+//  Created by alumno on 10/7/24.
+//
+
+import UIKit
+
+private let identificador_de_celda = "celda_pantalla_principal"
+
+class ControladorPantallaPrincipalDeColeccion: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Register cell classes
+        //self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: identificador_de_celda)
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using [segue destinationViewController].
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    // MARK: UICollectionViewDataSource
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 2
+    }
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 10
+    }
+    
+    // Función paraa identificar y crear cada una de las celdas creadas en el Controlador
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let celda: VistaDeZelda = collectionView.dequeueReusableCell(withReuseIdentifier: identificador_de_celda, for: indexPath) as! VistaDeZelda
+        
+        // Configure the cell
+        //celda.backgroundColor = UIColor.green //Se puede poner con un solo punto para indicar que es un color
+        
+        celda.etiqueta.text = "\(indexPath)"
+        
+        return celda
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath){
+        print("Se selaccionó la celda\(indexPath)")
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
---
+++ boceto2_191168/boceto2_191168/controladores_de_pantallas/vista_de_celdas/VistaDeZelda.swift
@@ -0,0 +1,14 @@
+//
+//  VistaDeZeldaCollectionViewCell.swift
+//  boceto2_191168
+//
+//  Created by alumno on 10/7/24.
+//
+
+import UIKit
+
+class VistaDeZelda: UICollectionViewCell {
+    @IBOutlet weak var etiqueta: UILabel!
+    
+    
+}
