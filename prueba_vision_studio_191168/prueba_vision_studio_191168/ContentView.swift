3d9ee90f6a3dc8c924220c1f4f9ee899d4ce58d5
alumno <alumno@lab_quimica.uacj.mx>
2024-10-16 10:40:08 -0600

    pruebas para Vision Pro

--- prueba_vision_studio_191168/prueba_vision_studio_191168/ContentView.swift
+++ prueba_vision_studio_191168/prueba_vision_studio_191168/ContentView.swift
@@ -10,12 +10,21 @@ import RealityKit
 import RealityKitContent
 
 struct ContentView: View {
+    @State var pulsaciones: Bool = true
+    
     var body: some View {
-        VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
-
-            Text("Hello, world!")
+        HStack {
+            VStack {
+                Model3D(named: "Scene", bundle: realityKitContentBundle)
+                    .padding(.bottom, 50)
+                Model3D(named: "prueba_2", bundle: realityKitContentBundle).padding(.bottom, 0)
+            }
+            Text("AAAAAA")
+            Text("AAAAAA")
+            Text("AAAAAA")
+            Button(action: {pulsaciones = !pulsaciones
+                print("Hola mundo desde consola\(pulsaciones)")
+            }, label: {Text("Hola Mundo")})
         }
         .padding()
     }
